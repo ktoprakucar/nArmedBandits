16bc09fe8c89f2079e1d4b2530fd7796fc40d5f9
Kemal Toprak Uçar <toprak.ucar@sadeyazilim.com>
2017-03-15 16:54:59 +0300

    fix: random selection if there are more than max value in greedySelection

--- src/main/java/Bandit.java
+++ src/main/java/Bandit.java
@@ -3,32 +3,42 @@ import Entity.EstimateValue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by toprak on 3/12/2017.
  */
 public class Bandit {
-    List<EstimateValue> estimateValues = new ArrayList<EstimateValue>();
-    List<Bag> bags = new ArrayList<Bag>();
-    double alpha = 0.0;
+  List<EstimateValue> estimateValues = new ArrayList<EstimateValue>();
+  Random generator = new Random();
+  List<Bag> bags = new ArrayList<Bag>();
+  double alpha = 0.0;
 
-    public int greedySelection(){
-        EstimateValue bagWithGreatestEstimate = estimateValues.get(0);
+  public int greedySelection() {
+    List<EstimateValue> maxValues = new ArrayList<EstimateValue>();
 
-        for(EstimateValue estimate : estimateValues){
-            if(estimateValues.indexOf(estimate) == 0){
-                bagWithGreatestEstimate = estimate;
-                continue;
-            }
-            if(estimate.estimateValue > bagWithGreatestEstimate.estimateValue)
-                bagWithGreatestEstimate = estimate;
-        }
-        return estimateValues.indexOf(bagWithGreatestEstimate);
+    for (EstimateValue estimate : estimateValues) {
+      if (estimateValues.indexOf(estimate) == 0) {
+        maxValues.add(estimateValues.get(0));
+        continue;
+      } else if (estimate.estimateValue == maxValues.get(0).estimateValue)
+        maxValues.add(estimate);
+      else if (estimate.estimateValue > maxValues.get(0).estimateValue) {
+        maxValues.clear();
+        maxValues.add(estimate);
+      }
     }
-
-    public void updateEstimateValue(int bagId, double reward){
-        EstimateValue estimateValue = estimateValues.get(bagId);
-        estimateValue.updateEstimateValue(reward);
-
+    if (maxValues.size() == 1)
+      return estimateValues.indexOf(maxValues.get(0));
+    else {
+      int randomEstimateIndex = generator.nextInt((maxValues.size() - 1) - 0 + 1) + 0;
+      return estimateValues.indexOf(maxValues.get(randomEstimateIndex));
     }
+  }
+
+  public void updateEstimateValue(int bagId, double reward) {
+    EstimateValue estimateValue = estimateValues.get(bagId);
+    estimateValue.updateEstimateValue(reward);
+
+  }
 }
--- src/test/java/BanditTest.java
+++ src/test/java/BanditTest.java
@@ -3,38 +3,76 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.AnyOf.anyOf;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by toprak on 3/12/2017.
  */
 public class BanditTest {
-    Bandit bandit;
+  Bandit bandit;
 
-    @Before
-    public void setUp() {
-        bandit = new Bandit();
-        EstimateValue e1 = new EstimateValue(0.0);
-        e1.estimateValue = 4.0;
+  @Before
+  public void setUp() {
+    bandit = new Bandit();
+  }
 
-        EstimateValue e2 = new EstimateValue(0.0);
-        e2.estimateValue = 5.0;
+  @Test
+  public void test_choose_highest_rewarded_bag_with_different_values() {
+    EstimateValue e1 = new EstimateValue(0.0);
+    e1.estimateValue = 4.0;
 
-        EstimateValue e3 = new EstimateValue(0.0);
-        e3.estimateValue = 6.0;
+    EstimateValue e2 = new EstimateValue(0.0);
+    e2.estimateValue = 5.0;
 
-        EstimateValue e4 = new EstimateValue(0.0);
-        e4.estimateValue = 7.0;
+    EstimateValue e3 = new EstimateValue(0.0);
+    e3.estimateValue = 6.0;
 
+    EstimateValue e4 = new EstimateValue(0.0);
+    e4.estimateValue = 7.0;
 
-        bandit.estimateValues.add(e1);
-        bandit.estimateValues.add(e2);
-        bandit.estimateValues.add(e3);
-        bandit.estimateValues.add(e4);
-    }
+    bandit.estimateValues.add(e1);
+    bandit.estimateValues.add(e2);
+    bandit.estimateValues.add(e3);
+    bandit.estimateValues.add(e4);
 
-    @Test
-    public void test_choose_highest_rewarded_bag() {
-        int index = bandit.greedySelection();
-        assertEquals(3, index);
-    }
+    int index = bandit.greedySelection();
+    assertEquals(3, index);
+  }
+
+  @Test
+  public void test_choose_highest_rewarded_bag_with_replicated_values() {
+    EstimateValue e1 = new EstimateValue(0.0);
+    e1.estimateValue = 4.0;
+
+    EstimateValue e2 = new EstimateValue(0.0);
+    e2.estimateValue = 4.0;
+
+    EstimateValue e3 = new EstimateValue(0.0);
+    e3.estimateValue = 4.0;
+
+    EstimateValue e4 = new EstimateValue(0.0);
+    e4.estimateValue = 7.0;
+
+    EstimateValue e5 = new EstimateValue(0.0);
+    e5.estimateValue = 7.0;
+
+    EstimateValue e6 = new EstimateValue(0.0);
+    e6.estimateValue = 7.0;
+
+    EstimateValue e7 = new EstimateValue(0.0);
+    e7.estimateValue = 6.0;
+
+    bandit.estimateValues.add(e1);
+    bandit.estimateValues.add(e2);
+    bandit.estimateValues.add(e3);
+    bandit.estimateValues.add(e4);
+    bandit.estimateValues.add(e5);
+    bandit.estimateValues.add(e6);
+    bandit.estimateValues.add(e7);
+
+    int index = bandit.greedySelection();
+    assertThat(index, anyOf(is(3), is(4), is(5)));
+  }
 }
